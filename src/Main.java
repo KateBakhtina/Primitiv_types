b63002920e12677c8c2eac2f269e8ad47ba701db
Kate Bakhtina <almazniegori@gmail.com>
2024-10-07 23:50:16 +0300

    initial_commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задание 1 — обязательное
+
+        int ticketPrice = 13_676;
+        int quantityRubles = 20;
+
+        int result = ticketPrice / quantityRubles;
+        System.out.println("Начислено " + result + " бонуса(ов)");
+    }
+}
